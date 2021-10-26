deab0259db84096823a3392dd6af071976cc09e4
Ewa <ewa.rosset@softwire.com>
2021-10-26 15:13:34 +0100

    Cleaned up merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,36 @@
-<<<<<<< Updated upstream
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 import './App.scss';
 import Navbar from './components/navbar/navbar';
 import Home from './views/Home';
-=======
-import React, {useEffect, useState } from 'react';
 import './App.scss';
->>>>>>> Stashed changes
-import { DailyPhoto } from './components/Potd/Potd.component';
 
 
 function App() {
 
-  
-
-  return (
-    <div className="App">
-<<<<<<< Updated upstream
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/earth">
-            <p>Earth</p>
-          </Route>
-          <Route path="/mars">
-            <p>Mars</p>
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-=======
-      <DailyPhoto/>
->>>>>>> Stashed changes
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Navbar/>
+                <Switch>
+                    <Route path="/earth">
+                        <p>Earth</p>
+                    </Route>
+                    <Route path="/mars">
+                        <p>Mars</p>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
