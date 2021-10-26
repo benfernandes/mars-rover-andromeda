fdc9bccea4ccf3d4fcf6245cdcbcf668b45951de
Richard Sutch <richard.sutch@softwire.com>
2021-10-26 09:31:23 +0100

    Created React Router structure and the Navbar element

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.scss';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/earth">
+            <p>Earth</p>
+          </Route>
+          <Route path="/mars">
+            <p>Mars</p>
+          </Route>
+          <Route path="/">
+            <p>Home</p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
