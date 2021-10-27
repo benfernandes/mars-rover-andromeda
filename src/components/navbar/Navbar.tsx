8ba3bf2cd2000a7ed786110e98b5087a0af57bbd
Richard Sutch <richard.sutch@softwire.com>
2021-10-27 13:36:52 +0100

    Added different navbars for desktop vs mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,19 +3,15 @@ import { Link } from "react-router-dom";
 import { GoThreeBars } from "react-icons/go";
 import './Navbar.scss';
 
+interface Clickable {
+    onClick: () => void
+}
+
 function Navbar() {
     const [navbarOpen, setNavBarOpen] = useState(false);
 
-    function toggleNavbar() {
-        console.log("toggle");
-        setNavBarOpen(current => !current);
-    }  
-
-    return (
-        <nav className={`navbar ${navbarOpen ? "open" : "closed"}`}>
-            <button className="nav-button hamburger-button" onClick={toggleNavbar}>
-                <GoThreeBars color="white" />
-            </button>
+    function Links(props:Clickable) {
+        return (
             <ul className="nav-list" >
                 <li>
                     <Link className="nav-button" onClick={toggleNavbar} to="/">
@@ -34,8 +30,67 @@ function Navbar() {
                         <img className="planet-icon" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/1556252/planet-mars-clipart-xl.png" width="25" alt="mars" />
                     </Link>
                 </li>
+                <li>
+                    <Link className="nav-button" onClick={toggleNavbar} to="/">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-button" onClick={toggleNavbar} to="/earth">
+                        Earth
+                        <img className="planet-icon" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/59788/earth-planet-clipart-md.png" width="25" alt="earth" />
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-button" onClick={toggleNavbar} to="/mars">
+                        Mars
+                        <img className="planet-icon" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/1556252/planet-mars-clipart-xl.png" width="25" alt="mars" />
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-button" onClick={toggleNavbar} to="/">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-button" onClick={toggleNavbar} to="/earth">
+                        Earth
+                        <img className="planet-icon" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/59788/earth-planet-clipart-md.png" width="25" alt="earth" />
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-button" onClick={toggleNavbar} to="/mars">
+                        Mars
+                        <img className="planet-icon" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/1556252/planet-mars-clipart-xl.png" width="25" alt="mars" />
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-button" onClick={toggleNavbar} to="/earth">
+                        Earth
+                        <img className="planet-icon" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/59788/earth-planet-clipart-md.png" width="25" alt="earth" />
+                    </Link>
+                </li>
             </ul>
-        </nav>
+        );
+    }
+    
+    function toggleNavbar() {
+        console.log("toggle");
+        setNavBarOpen(current => !current);
+    }
+
+    return (
+        <div>
+            <nav className={`mobile-navbar ${navbarOpen ? "open" : "closed"}`}>
+                <button className="nav-button hamburger-button" onClick={toggleNavbar}>
+                    <GoThreeBars color="white" />
+                </button>
+                <Links onClick={toggleNavbar}/>
+            </nav>
+            <nav className="desktop-navbar">
+                <Links onClick={() => {}}/>
+            </nav>
+        </div>
     );
 }
 
