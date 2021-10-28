51817ee08b3a3a8b071ac88b667cd47b51e638d1
Aaron Spencer <aaronspencer@softwire.com>
2021-10-28 11:05:06 +0100

    Test and API key
    API Key moved to environment variables(ask if you need this doing on your machine).
    Test created to make sure the MarsRoverAPI returns images by quantity needed.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { GetImagesByQuantity } from './api/MarsRoverAPI';
 
 test('app renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
 });
+
+test('MarsRover Api returns the correct amount of img URLs', async () => {
+    const urlArray = await GetImagesByQuantity(100);
+    expect(urlArray.length).toBe(100);
+})
+
+
