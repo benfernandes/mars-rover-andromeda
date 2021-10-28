4249b74244f1e494ed364417e6498688c38ba76d
Temuujin Natsagnyam <temuujin.natsagnyam@softwire.com>
2021-10-28 16:06:57 +0100

    2 sections made

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import scrollDown from "../images/scrolldown.png"
+import "./Home.scss"
+
 
 function Home() {
     return (
-        <div>
-            <p>Homepage</p>
-        </div>
+        <main className="home-page">
+            <section className="first-section">
+                <div className="section-title">
+                    EXPLORE
+                    <div className="section-subtitle">THE KNOWN UNIVERSE</div>
+                </div>
+                {/* <PhotoOfTheDay /> */}
+                <div className="scroll-down-wrapper">
+                    <img src={scrollDown} className="scroll-down"/>
+                </div>
+            </section>
+            <section className="last-section">
+                <div className="section-title">
+                    MAR
+                    <div className="section-subtitle">MARCH AWAKENS ROMANCE</div>
+                </div>
+            </section>   
+        </main>
     )
 }
 
