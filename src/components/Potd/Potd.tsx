4865f9945e54169a263a511a0b8dbb9d2694422f
Ewa <ewa.rosset@softwire.com>
2021-10-28 18:01:30 +0100

    Remove spaces

--- src/components/Potd/Potd.tsx
+++ src/components/Potd/Potd.tsx
@@ -1,30 +1,24 @@
-import {useEffect, useState } from "react";
-import {GetDataMainPhoto, iPotdData, iPotdVideoData } from "../../api/Potd";
+import {useEffect, useState} from "react";
+import {GetDataMainPhoto, iPotdData, iPotdVideoData} from "../../api/Potd";
 import './Potd.scss';
-// import VideoThumbnail from 'react-video-thumbnail';
 
-export function PhotoOrVideo(videoPotd:iPotdVideoData){
-    if (videoPotd.media_type == "video"){
-
-
-        return(
+export function PhotoOrVideo(videoPotd: iPotdVideoData) {
+    if (videoPotd.media_type == "video") {
+        return (
             <iframe
-                className="DailyImage" src={videoPotd.url+(videoPotd.url.includes("?") ? "&" : "?") + "controls=0"}>
+                className="DailyImage" src={videoPotd.url + (videoPotd.url.includes("?") ? "&" : "?") + "controls=0"}>
             </iframe>
-
         )
     }
 
     return (
-
         <img className="DailyImage" src={videoPotd.url}></img>
     )
 }
 
+export function DailyPhoto() {
 
-export function DailyPhoto(){
-
-    const[dataPotd, dataPotdSet] = useState<iPotdData|null>(null);
+    const [dataPotd, dataPotdSet] = useState<iPotdData | null>(null);
 
     useEffect(() => {
         GetDataMainPhoto.then((response) => {
@@ -42,20 +36,15 @@ export function DailyPhoto(){
 
     return (
         <div className="PhotoOfTheDay">
-
             <section className="ImageSection">
-                <PhotoOrVideo media_type={dataPotd.media_type} url={dataPotd.url} />
+                <PhotoOrVideo media_type={dataPotd.media_type} url={dataPotd.url}/>
                 <section className="MetadataSection">
                     <div className={"ImageTitle"}>{dataPotd.title}</div>
                 </section>
             </section>
-
             <section className="ExplanationSection">
                 <div className="ExplanationText">{dataPotd.explanation}</div>
             </section>
-
-
-
         </div>
     );
-    }
+}
