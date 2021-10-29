bb8dbcf109d3a7999ceed5da7a0ff15e95676ce0
Ewa <ewa.rosset@softwire.com>
2021-10-29 12:25:20 +0100

    Initial Earth Page details

---
+++ src/views/Earth.tsx
@@ -0,0 +1,18 @@
+import "./Home.scss"
+// import EarthSatteliteFunction
+
+function Earth() {
+    return (
+        <main className="home-page">
+            <section className="last-section">
+                <div className="section-title">
+                    Earth
+                    <div className="section-subtitle">VIEW PHOTOS OF EARTH!</div>
+                </div>
+                {/* <EarthSatteliteFunction/> */}
+            </section>
+        </main>
+    )
+}
+
+export default Earth;
