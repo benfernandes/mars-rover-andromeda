71e88ace208ecf4ff2e258a7ecc226ce94099425
Temuujin Natsagnyam <temuujin.natsagnyam@softwire.com>
2021-10-29 02:57:04 +0100

    Mars section added to homepage

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import scrollDown from "../images/scrolldown.png"
 import earthImg from "../images/earth.png"
+import marsImg from "../images/mars.png"
 import "./Home.scss"
 
 
@@ -22,6 +23,11 @@ function Home() {
                     MAR
                     <div className="section-subtitle">MARCH AWAKENS ROMANCE</div>
                 </div>
+                <Link to="/earth">
+                    <img className="mars-icon" src={marsImg} />
+                </Link>
+                <div className="section-subtitle">SEE IMAGES CAPTURED BY ROVERS ON MARS, BY TELESCOPES FROM EARTH ETC.</div>
+                <Link to="/mars" className="eightbit-btn">GO TO MARS {'>>>'}</Link>
             </section>   
             <Link to="/earth">
                 <img className="earth-icon" src={earthImg} />
