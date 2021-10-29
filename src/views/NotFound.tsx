316bd4b362e048ce830e7abdf77448eed448d84f
Richard Sutch <richard.sutch@softwire.com>
2021-10-29 15:52:40 +0100

    Added 404 page styles

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import './NotFound.scss';
 
 function NotFound() {
     return (
-        <div>
+        <div className="not-found-div">
             <h1>Error 404 - Page not found</h1>
             <Link to="./">Go home</Link>
         </div>
