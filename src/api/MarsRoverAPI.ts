3eca2ee382bb8c76561332baab1004bda94aa9d9
Aaron Spencer <aaronspencer@softwire.com>
2021-10-29 10:54:22 +0100

    git Ignore updated

--- src/api/MarsRoverAPI.ts
+++ src/api/MarsRoverAPI.ts
@@ -1,4 +1,3 @@
-// Api key - t7bN4gPSJzbQidlv33BJnyY2IT8dD3d0KuDztdWz
 
 // As we scale we can offer options of rover, camera angle or date search params.
 enum Rover{
@@ -7,39 +6,29 @@ enum Rover{
     Spirit = 'spirit'
 }
 
-async function GetImagesByQuantity(quantity: number) {
-    // const ApiContext = useContext(ApiCtx);
-    const apiKey = 't7bN4gPSJzbQidlv33BJnyY2IT8dD3d0KuDztdWz';
+export async function GetImagesByQuantity(quantity: number) {
     let startSol = quantity * 1.2; // we are giving the API more days than pictures we're looking for to make sure we return enough.
     let apiCall: string = `https://api.nasa.gov/mars-photos/api/v1/rovers/${Rover.Curiosity}/photos?sol=${startSol}&api_key=${process.env.REACT_APP_MARS_ROVER_API_KEY}`;
 
     let resultsArray : string[] = new Array(quantity).fill('');
     let toJson;
-    let arrayLength = 0;
-    
-    while(arrayLength < quantity)
-    {    
+    let returnedImagesArrayLength = 0;
+
+    // The API does not allow for requesting a specific quantity of images, to get around this we make sure we get more than we need.
+    // We then only populate the results array with how many we need.
+    while(returnedImagesArrayLength < quantity) {
         const response = await fetch(apiCall);
         toJson = await response.json();
         startSol += 10; // we're going to increase this till we have enough photos.
-        arrayLength = toJson.photos.length;
+        returnedImagesArrayLength = toJson.photos.length;
     }
 
-    for(let i = 0; i < quantity; i ++)
-    {
+    for(let i = 0; i < quantity; i ++) {
         resultsArray[i] = toJson.photos[i].img_src;
     }
 
-
     return resultsArray;
-
-    
-
 }
 
-
-
-
-export {GetImagesByQuantity};
 
 
